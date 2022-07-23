d5370106916c4fdb0d6b99d13f34208cbf2c50d0
wahyusrp <setio101199@gmail.com>
2022-07-24 00:13:03 +0700

    Versioning source code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include "adder.h"
 #include "GLFW/glfw3.h"
+#include "TutorialConfig.h"
 
-int main()
+int main(int argc, char* argv[])
 {
+
+	std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."<< Tutorial_VERSION_MINOR << "\n";
+
 	GLFWwindow *window;
 	if(!glfwInit())
 		exit(EXIT_FAILURE);

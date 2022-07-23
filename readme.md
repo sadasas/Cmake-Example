2132aea46566a0f07d1dbdd686f22d888d7b7584
wahyusrp <setio101199@gmail.com>
2022-07-23 21:31:24 +0700

    Create window gui with opengl

---
+++ readme.md
@@ -0,0 +1,7 @@
+step
+
+.\configure.bat
+.\build.bat
+.\run.bat
+
+enjoy my first project intregated with cmake 
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 #include "adder.h"
+#include "GLFW/glfw3.h"
 
 int main()
 {
-	std::cout << Add(2,1);
+	GLFWwindow *window;
+	if(!glfwInit())
+		exit(EXIT_FAILURE);
+	
+	window = glfwCreateWindow(400,400,"First",NULL,NULL);
+	glfwSwapInterval(1);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwWaitEvents();
+	}
+	glfwTerminate();
+	exit(EXIT_SUCCESS);
+
 	return 0;
 }
